e9545e643c4eebe1339b97a1096b7462571d5c19
Zeerak Baig <zeerak@bbd.co.za>
2023-04-15 09:55:35 +0200

    added basic db connection in weatherforcast controller

--- FoodDeliveryAPI/FoodDeliveryAPI/Controllers/WeatherForecastController.cs
+++ FoodDeliveryAPI/FoodDeliveryAPI/Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace FoodDeliveryAPI.Controllers
 {
@@ -12,15 +15,35 @@ namespace FoodDeliveryAPI.Controllers
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            // Retrieve the connection string from IConfiguration
+            string connectionString = _configuration.GetConnectionString("FoodDB");
+            List<WeatherForecast> weatherForecasts = new List<WeatherForecast>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Restaurant", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Create a new WeatherForecast object and populate its properties
+                            string restName = reader.GetString("restaurantUserName");
+                        }
+                    }
+                }
+            }
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
