10d03d2de7d315d09b56f1c63c0c0fb88834f7f0
Willa Lyle <willa@indiefin.com>
2023-04-20 16:50:40 +0200

    feat: add updaters

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using FoodDeliveryAPI.Models;
 using FoodDeliveryAPI.DatabaseAccess;
+using FoodDeliveryAPI.Updaters;
 
 namespace FoodDeliveryAPI.Controllers
 {
@@ -11,10 +12,24 @@ namespace FoodDeliveryAPI.Controllers
     public class OrderController : ControllerBase
     {
         private readonly SqlConnection _connection;
+        private readonly EventHandler<Updater.UpdateOrderStatusArgs> onUpdateOrderStatus = (sender, eventArgs) =>
+        {
+            using (SqlCommand command = new SqlCommand(
+                @"UPDATE [Order]
+                SET orderStatusId = @orderStatusId
+                WHERE orderId = @orderId", DBConnection.Instance.Connection
+            ))
+            {
+                command.Parameters.AddWithValue("@orderStatusId", eventArgs.OrderStatusId);
+                command.Parameters.AddWithValue("@orderId", eventArgs.ParentId);
+                command.ExecuteNonQuery();
+            }
+        };
 
         public OrderController()
         {
             _connection = DBConnection.Instance.Connection;
+            Updater.UpdateOrderStatusEvent += onUpdateOrderStatus;
         }
 
 
@@ -24,28 +39,28 @@ namespace FoodDeliveryAPI.Controllers
         {
 
             List<Order> orders = new List<Order>();
-           
-                using (SqlCommand command = new SqlCommand("SELECT * FROM [Order]", _connection))
+
+            using (SqlCommand command = new SqlCommand("SELECT * FROM [Order]", _connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
                 {
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
                     {
-                        while (reader.Read())
-                        {
-                            Order order = new Order();
+                        Order order = new Order();
 
-                            order.orderId = (int)reader["orderId"];
-                            order.orderDate = reader.GetDateTime("orderDate");
-                            order.restaurantId = (int)reader["restaurantId"];
-                            order.userId = (int)reader["userId"];
-                            order.personeelId = reader.GetInt32("personeelId");
-                            order.addressId = (int)reader["addressId"];
-                            order.orderStatusId = (int)reader["orderStatusId"];
+                        order.orderId = (int)reader["orderId"];
+                        order.orderDate = reader.GetDateTime("orderDate");
+                        order.restaurantId = (int)reader["restaurantId"];
+                        order.userId = (int)reader["userId"];
+                        order.personeelId = reader.GetInt32("personeelId");
+                        order.addressId = (int)reader["addressId"];
+                        order.orderStatusId = (int)reader["orderStatusId"];
 
-                            orders.Add(order);
-                        }
+                        orders.Add(order);
                     }
                 }
-           // }
+            }
+            // }
 
             return Ok(orders);
         }
@@ -54,26 +69,26 @@ namespace FoodDeliveryAPI.Controllers
         public ActionResult<Order> Get(int orderId)
         {
             Order order = new Order();
-                SqlCommand command = new SqlCommand("SELECT * FROM [Order] WHERE orderId = @orderId", _connection);
-                command.Parameters.AddWithValue("@orderId", orderId);
-                
-                SqlDataReader reader = command.ExecuteReader();
-                if (reader.Read())
-                {
-                    order.orderId = (int)reader["orderId"];
-                    order.orderDate = reader.GetDateTime("orderDate");
+            SqlCommand command = new SqlCommand("SELECT * FROM [Order] WHERE orderId = @orderId", _connection);
+            command.Parameters.AddWithValue("@orderId", orderId);
 
-                    order.restaurantId = (int)reader["restaurantId"];
-                    order.userId = (int)reader["userId"];
-                    order.personeelId = (int)reader["personeelId"];
-                    order.addressId = (int)reader["addressId"];
-                    order.orderStatusId = (int)reader["orderStatusId"];
-                }
-                else
-                {
-                    return NotFound();
-                }
-                reader.Close();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                order.orderId = (int)reader["orderId"];
+                order.orderDate = reader.GetDateTime("orderDate");
+
+                order.restaurantId = (int)reader["restaurantId"];
+                order.userId = (int)reader["userId"];
+                order.personeelId = (int)reader["personeelId"];
+                order.addressId = (int)reader["addressId"];
+                order.orderStatusId = (int)reader["orderStatusId"];
+            }
+            else
+            {
+                return NotFound();
+            }
+            reader.Close();
             return order;
         }
 
@@ -86,19 +101,19 @@ namespace FoodDeliveryAPI.Controllers
             {
                 return BadRequest();
             }
-                DateTime currentDateTime = DateTime.Now;
-                using (SqlCommand command = new SqlCommand("INSERT INTO [Order] (orderDate, restaurantId, userId, personeelId, addressId, orderStatusId)" +
-                "VALUES (@orderDate, @restaurantId, @userId, @personeelId, @addressId, @orderStatusId)", _connection))
-                {
-                    command.Parameters.AddWithValue("@orderDate", currentDateTime);
-                    command.Parameters.AddWithValue("@restaurantId", order.restaurantId);
-                    command.Parameters.AddWithValue("@userId", order.userId);
-                    command.Parameters.AddWithValue("@personeelId", order.personeelId);
-                    command.Parameters.AddWithValue("@addressId", order.addressId);
-                    command.Parameters.AddWithValue("@orderStatusId", order.orderStatusId);
-                    command.ExecuteNonQuery();
-                }
-           
+            DateTime currentDateTime = DateTime.Now;
+            using (SqlCommand command = new SqlCommand("INSERT INTO [Order] (orderDate, restaurantId, userId, personeelId, addressId, orderStatusId)" +
+            "VALUES (@orderDate, @restaurantId, @userId, @personeelId, @addressId, @orderStatusId)", _connection))
+            {
+                command.Parameters.AddWithValue("@orderDate", currentDateTime);
+                command.Parameters.AddWithValue("@restaurantId", order.restaurantId);
+                command.Parameters.AddWithValue("@userId", order.userId);
+                command.Parameters.AddWithValue("@personeelId", order.personeelId);
+                command.Parameters.AddWithValue("@addressId", order.addressId);
+                command.Parameters.AddWithValue("@orderStatusId", order.orderStatusId);
+                command.ExecuteNonQuery();
+            }
+
             return Ok();
         }
 
@@ -110,8 +125,8 @@ namespace FoodDeliveryAPI.Controllers
                 return BadRequest();
             }
             DateTime currentDateTime = DateTime.Now;
-    
-                string query = @"UPDATE [Order]
+
+            string query = @"UPDATE [Order]
                        SET restaurantId = @restaurantId,
                            orderDate = @orderDate,
                            userId = @userId,
@@ -120,19 +135,19 @@ namespace FoodDeliveryAPI.Controllers
                            orderStatusId = @orderStatusId
                        WHERE orderId = @orderId";
 
-                using (SqlCommand command = new SqlCommand(query, _connection))
-                {
-                    command.Parameters.AddWithValue("@orderId", id);
-                    command.Parameters.AddWithValue("@orderDate", currentDateTime);
-                    command.Parameters.AddWithValue("@restaurantId", order.restaurantId);
-                    command.Parameters.AddWithValue("@userId", order.userId);
-                    command.Parameters.AddWithValue("@personeelId", order.personeelId);
-                    command.Parameters.AddWithValue("@addressId", order.addressId);
-                    command.Parameters.AddWithValue("@orderStatusId", order.orderStatusId);
-                    command.ExecuteNonQuery();
+            using (SqlCommand command = new SqlCommand(query, _connection))
+            {
+                command.Parameters.AddWithValue("@orderId", id);
+                command.Parameters.AddWithValue("@orderDate", currentDateTime);
+                command.Parameters.AddWithValue("@restaurantId", order.restaurantId);
+                command.Parameters.AddWithValue("@userId", order.userId);
+                command.Parameters.AddWithValue("@personeelId", order.personeelId);
+                command.Parameters.AddWithValue("@addressId", order.addressId);
+                command.Parameters.AddWithValue("@orderStatusId", order.orderStatusId);
+                command.ExecuteNonQuery();
 
-                }
-            
+            }
+
             return Ok("Order Updated Successfully");
         }
     }
---
+++ Updaters/ItemStatusUpdater.cs
@@ -0,0 +1,39 @@
+using FoodDeliveryAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryAPI.Updaters
+{
+    public partial class Updater
+    {
+        public class UpdateItemStatusArgs : EventArgs
+        {
+            public int ItemStatusId { get; }
+            public int ParentId { get; }
+
+            public UpdateItemStatusArgs(int itemStatusId, int parentId)
+            {
+                ItemStatusId = itemStatusId;
+                ParentId = parentId;
+            }
+        }
+        public static event EventHandler<UpdateItemStatusArgs>? UpdateItemStatusEvent;
+
+        [HttpPost("itemStatus/{parentId}")]
+        public bool UpdateItemStatus([FromBody] string status, int parentId)
+        // public bool UpdateItemStatus(string status, int parentId)
+        {
+            int itemStatusId = ItemStatusController.GetIdByName(status);
+            if (itemStatusId != 0)
+            {
+                RaiseUpdateItemStatusEvent(itemStatusId, parentId);
+                return true;
+            }
+            else return false;
+        }
+
+        private void RaiseUpdateItemStatusEvent(int itemStatusId, int parentId)
+        {
+            UpdateItemStatusEvent?.Invoke(this, new UpdateItemStatusArgs(itemStatusId, parentId));
+        }
+    }
+}
