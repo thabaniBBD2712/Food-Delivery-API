efd23cf649062411371436c9dfcbe2c78b5d73c2
Willa Lyle <willa@indiefin.com>
2023-04-20 18:11:23 +0200

    Revert "updated the order processor and removed the updateOrder"
    This reverts commit 270961e1ccf3be5252102c12bbbc91e92e358f7e.

--- OrderProcessor.cs
+++ OrderProcessor.cs
@@ -45,6 +45,11 @@ public class OrderProcessor : IHostedService
                              command.Parameters.AddWithValue("@itemInformationId", response.itemInformationId);
                              command.ExecuteNonQuery();
                              Console.WriteLine("Checkout successful");
+                             if (response.orderId != null)
+                             {
+                                 string status = updateOrderStatus(connection, (int)response.orderId);
+                                 Console.WriteLine(status);
+                             }
                          }
                      }
                  }
@@ -56,5 +61,52 @@ public class OrderProcessor : IHostedService
     public Task StopAsync(CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
+    }
+
+    private string updateOrderStatus(SqlConnection connection, int orderId)
+    {
+        Order order = new Order();
+        SqlCommand command = new SqlCommand("SELECT * FROM [Order] WHERE orderId = @orderId", connection);
+        command.Parameters.AddWithValue("@orderId", orderId);
+        SqlDataReader reader = command.ExecuteReader();
+        //get the particular order
+        if (reader.Read())
+        {
+            order.orderId = (int)reader["orderId"];
+            order.orderDate = reader.GetDateTime("orderDate");
+            order.restaurantId = (int)reader["restaurantId"];
+            order.userId = (int)reader["userId"];
+            order.personeelId = (int)reader["personeelId"];
+            order.addressId = (int)reader["addressId"];
+            order.orderStatusId = (int)reader["orderStatusId"];
+        }
+        else
+        {
+            return "Order Not Found";
+        }
+
+        // update that order's status to pending
+        string query = @"UPDATE [Order]
+                       SET restaurantId = @restaurantId,
+                           orderDate = @orderDate,
+                           userId = @userId,
+                           personeelId = @personeelId,
+                           addressId = @addressId,
+                           orderStatusId = @orderStatusId
+                       WHERE orderId = @orderId";
+
+        using (SqlCommand updateCommand = new SqlCommand(query, connection))
+        {
+            updateCommand.Parameters.AddWithValue("@orderId", order.orderId);
+            updateCommand.Parameters.AddWithValue("@orderDate", order.orderDate);
+            updateCommand.Parameters.AddWithValue("@restaurantId", order.restaurantId);
+            updateCommand.Parameters.AddWithValue("@userId", order.userId);
+            updateCommand.Parameters.AddWithValue("@personeelId", order.personeelId);
+            updateCommand.Parameters.AddWithValue("@addressId", order.addressId);
+            updateCommand.Parameters.AddWithValue("@orderStatusId", 1);
+            updateCommand.ExecuteNonQuery();
+        }
+        reader.Close();
+        return "Your order is Pending and will be delivered shortly";
     }
 }
